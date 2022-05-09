446460e3bd27d62d7419a960c9203b9377ef47d6
nyaribari <anyaribari@gmail.com>
2022-05-09 13:32:10 +0300

    creating components and pages and new css files

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Landing from '../components/landing/Landing'
+
+const HomePage = () => {
+  return (
+      <>
+    <div>HomePage</div>
+    <Landing/>
+    <div>End page</div>
+      </>
+  )
+}
+
+export default HomePage
